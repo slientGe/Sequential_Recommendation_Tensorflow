03e87b1c41fecb7141bbe9b0978b5df70d309086
XiufengShu <15619319228@163.com>
2020-06-29 19:10:44 +0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,40 +1,6 @@
 import numpy as np
 import math
 
-def SortItemsbyScore(item_list, item_score_list, reverse=False,remove_hist=False, usr = None, usrclick = None):
-
-    totals = len(item_score_list)
-    result_items = []
-    result_score = []
-    for i in range(totals):
-        u = usr[i]
-        u_clicks = usrclick[u]
-        item_score = item_score_list[i]
-        tuple_list = sorted(list(zip(item_list,item_score)),key=lambda x:x[1],reverse=reverse)
-
-        if remove_hist:
-            tmp = []
-            for t in tuple_list:
-                if t[0] not in u_clicks:
-                    tmp.append(t)
-            tuple_list = tmp
-
-        x, y = zip(*tuple_list)
-        sorted_item = list(x)
-        sorted_score = list(y)
-        result_items.append(sorted_item)
-        result_score.append(sorted_score)
-
-    return result_items,result_score
-
-
-
-
-
-
-
-
-
 def Metric_PrecN(target_list, predict_list, num):
 
     sum = 0
@@ -98,3 +64,29 @@ def Metric_MRR(target_list, predict_list):
             sums += 1 / rank
             count += 1
     return float(sums) / count
+
+def SortItemsbyScore(item_list, item_score_list, reverse=False,remove_hist=False, usr = None, usrclick = None):
+
+    totals = len(item_score_list)
+    result_items = []
+    result_score = []
+    for i in range(totals):
+        u = usr[i]
+        u_clicks = usrclick[u]
+        item_score = item_score_list[i]
+        tuple_list = sorted(list(zip(item_list,item_score)),key=lambda x:x[1],reverse=reverse)
+
+        if remove_hist:
+            tmp = []
+            for t in tuple_list:
+                if t[0] not in u_clicks:
+                    tmp.append(t)
+            tuple_list = tmp
+
+        x, y = zip(*tuple_list)
+        sorted_item = list(x)
+        sorted_score = list(y)
+        result_items.append(sorted_item)
+        result_score.append(sorted_score)
+
+    return result_items,result_score
